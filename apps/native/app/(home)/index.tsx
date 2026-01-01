455e57705f1eca96201afb99d1250638147b9cae
ahjaradithya <ahzathenation@gmail.com>
2026-01-01 08:42:58 +0700

    adding (home) dir

---
+++ apps/native/app/(home)/index.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="font-bold text-4xl">Welcome to Lumira!</h1>
+      <p className="text-muted-foreground text-xl">
+        Start building your app here.
+      </p>
+    </div>
+  );
+}
